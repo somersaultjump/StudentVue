5d05f5dc4eb5c84570953ed913622f41b27841b3
somersaultjump <daniel@dljurek.com>
2023-07-24 16:23:57 -0700

    adding minimum to connect to service

--- ParentVue.py
+++ ParentVue.py
@@ -1,3 +1,25 @@
 import argparse
 import json
 from studentvue import StudentVue
+
+parser = argparse.ArgumentParser(description='ParentVUE interface script.')
+parser.add_argument("-u","--username", help="Provide username.")
+parser.add_argument("-p","--password", help="Provide password.")
+parser.add_argument("-d","--district", help="Provide district URL to connect to.", required=True)
+parser.add_argument("--debug",
+    help="Show raw payload from ParentVUE.",
+    action="store_true")
+args = parser.parse_args()
+
+user = args.username
+passw = args.password
+domain = args.district
+connection = StudentVue(user, passw, domain)
+
+record = connection.get_student_info()
+
+if (args.debug):
+    print('DEBUG: Get raw payload from ParentVue...')
+    record = connection.get_student_info()
+    print(json.dumps(record, indent=4))
+    raise SystemExit()
